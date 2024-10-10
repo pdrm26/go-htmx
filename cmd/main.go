ba67b12bff952518cd84943b3e524df260ab3cd9
pedram <peeeedii@gmail.com>
2024-10-10 21:02:35 +0330

    feat: create a simple user form

--- cmd/main.go
+++ cmd/main.go
@@ -22,23 +22,52 @@ func newTemplate() *Templates {
 	}
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func newData() Data {
+
+	return Data{
+		Contacts: []Contact{
+			newContact("Jack", "j@gmail.com"),
+			newContact("Pedram", "p@gmail.com"),
+		},
+	}
+
 }
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
-	count := Count{Count: 0}
 	e.Renderer = newTemplate()
 
+	data := newData()
+
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", data)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		data.Contacts = append(data.Contacts, newContact(name, email))
+		return c.Render(200, "index", data)
 	})
 
 	e.Logger.Fatal(e.Start(":5208"))
